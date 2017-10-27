1a11908b8e6352b955318a9e34bdd70e705acbcb
LC <348722306@qq.com>
2017-10-28 01:17:23 +0800

    1028：mybatis以及 converts（正在研究）

--- spring-db/src/main/java/com/lc/spring/db/config/DBConfiguration.java
+++ spring-db/src/main/java/com/lc/spring/db/config/DBConfiguration.java
@@ -1,16 +1,11 @@
 package com.lc.spring.db.config;
 
-import com.lc.spring.db.convert.LocalDateConverter;
 import com.lc.spring.db.convert.MyHttpMessageConvert;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ConversionServiceFactoryBean;
-import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.time.LocalDate;
 import java.util.List;
 
 /**
