54ca23f9a93cd02f5a2e940322eca89d4807a53a
liangchao-ubuntu <348722306@qq.com>
2017-11-04 11:48:23 +0800

    11.04: config server初始化

---
+++ spring-config-server/src/main/java/com/lc/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.lc.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by liangchao on 17-11-4.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
