f66ac1776122da878b5487351c37a78abaa7abcd
colter <liangchao03@ppdai.com>
2018-01-16 11:25:03 +0800

    自定义PropertiesLoader

--- spring-db/src/main/java/com/lc/spring/db/loader/CustomPropertySourceLoader2.java
+++ spring-db/src/main/java/com/lc/spring/db/loader/CustomPropertySourceLoader2.java
@@ -1,6 +1,6 @@
 package com.lc.spring.db.loader;
 
-import org.springframework.boot.env.PropertySourceLoader;
+import org.springframework.boot.env.PropertiesPropertySourceLoader;
 import org.springframework.core.PriorityOrdered;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
@@ -16,8 +16,11 @@ import java.util.Properties;
  * 自定义SourceLoader覆盖原来的SourceLoader
  * 因为实现了ProorityOrdered。实现这个接口的对象在不实现这个接口的类之前，所以能够覆盖。
  * spring.factories需要配置
+ *
+ * 对应dev用自己的方法，其他的用 默认的。
+ *
  */
-public class CustomPropertySourceLoader implements PropertySourceLoader,PriorityOrdered{
+public class CustomPropertySourceLoader2 extends PropertiesPropertySourceLoader implements PriorityOrdered{
     @Override
     public String[] getFileExtensions() {
         return new String[] { "properties", "xml" };
@@ -26,10 +29,14 @@ public class CustomPropertySourceLoader implements PropertySourceLoader,Priority
     @Override
     public PropertySource<?> load(String name, Resource resource, String profile) throws IOException {
         if (profile == null) {
-            EncodedResource encodedResource = new EncodedResource(resource, StandardCharsets.UTF_8);
-            Properties properties = PropertiesLoaderUtils.loadProperties(encodedResource);
-            if (!properties.isEmpty()) {
-                return new PropertiesPropertySource(name, properties);
+            if(name.equals("applicationConfig: [classpath:/application-dev.properties]")){
+                EncodedResource encodedResource = new EncodedResource(resource, StandardCharsets.UTF_8);
+                Properties properties = PropertiesLoaderUtils.loadProperties(encodedResource);
+                if (!properties.isEmpty()) {
+                    return new PropertiesPropertySource(name, properties);
+                }
+            }else{
+                return super.load(name,resource,profile);
             }
         }
         return null;
@@ -37,6 +44,6 @@ public class CustomPropertySourceLoader implements PropertySourceLoader,Priority
 
     @Override
     public int getOrder() {
-        return Integer.MAX_VALUE;
+        return Integer.MIN_VALUE;
     }
 }
